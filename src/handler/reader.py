6c4907a42aa566731842e0f8e4ceea5148941773
Chan, Steven <steven.chan@garmin.com>
2022-02-01 22:37:22 +0800

    implement algorithm

---
+++ src/handler/reader.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Reader:
+    def __init__(self, filepath: str):
+        self.wordlist = self.__load_file(filepath)
+        self.n_words = len(self.wordlist)
+
+    def __load_file(self, filepath: str) -> List[str]:
+        with open(filepath, 'r') as streamer:
+            return streamer.read().splitlines()
+
+    def fetch_all(self) -> List[str]:
+        return self.wordlist
+
+    def fetch_one(self, idx: int) -> str:
+        return self.wordlist[idx % self.n_words]
---
+++ src/handler/words.py
@@ -0,0 +1,45 @@
+import random
+from enum import Enum
+from typing import List
+
+from .reader import Reader
+
+
+class Color(Enum):
+    GREEN = 0
+    YELLOW = 1
+    GRAY = 2
+
+
+class WordAgent:
+    def __init__(self, wordlist: str):
+        self.reader = Reader(wordlist)
+        self.solution = self.choice()
+
+    def choice(self) -> str:
+        rnd_idx = random.randint(0, self.reader.n_words)
+        return self.reader.fetch_one(rnd_idx)
+
+    def solution(self) -> str:
+        return self.solution
+
+    def is_solution(self, guess: str) -> bool:
+        return guess == self.solution
+
+    def inspect(self, guess: str) -> List[Color]:
+        if len(self.solution) != len(guess):
+            raise ValueError(f'expected length of word {len(self.solution)}, got {len(guess)}')
+
+        return [
+            self.__get_color(guess_char, sol_char)
+            for guess_char, sol_char in zip(guess, self.solution)
+        ]
+
+    def __get_color(self, guess_char: str, sol_char: str) -> Color:
+        if guess_char == sol_char:
+            return Color.GREEN
+
+        if guess_char in self.solution:
+            return Color.YELLOW
+
+        return Color.GRAY
