f215d0200d674e68328c670109281051bcb9d444
chun_shou_liu <chunshou_liu@trendmicro.com>
2022-10-13 23:53:12 +0800

    add game color in logic

--- ui/all.js
+++ ui/all.js
@@ -1,32 +1,49 @@
 $(document).ready(function(){
-    const grid = document.querySelector(".grid");
+    // initial variables
     let pointer = [0, 0];
-    console.log(pointer[0], pointer[1]);
+    let guess = [];
+    const answer = ["g", "r", "e", "a", "t"]; // for testing
 
-    // console.log(grid.getAttribute("row", "col"))
+    // checkAnswer while press on #enter
+    function checkAnswer(guess, answer) {
+        for (i=0; i<answer.length; i++) {
+            if (answer[i]===guess[i]){
+                $(`.gamerow[row=${pointer[0]}] .grid[col=${i}]`).addClass('correct');
+            } else if (answer.includes(guess[i])) {
+                $(`.gamerow[row=${pointer[0]}] .grid[col=${i}]`).addClass('halfcorrect');
+            } else {
+                $(`.gamerow[row=${pointer[0]}] .grid[col=${i}]`).addClass('none');
+            }
+        }
+    }
+
+    // add keyboard style while clicked on 
     $('.keyboard').on('click', 'button', function(event) {
         $(this).parent().parent().find('button').removeClass('activate');
         $(this).addClass('activate');
-        
     });
 
+    // show letter in grid by keyboard value
     $('.keyboard').on('click', '.btn', function(event) {
         if (pointer[1] >= 5) {
             alert("please press enter or delete!");
         } else {
             event.preventDefault();
             let letter = event.target.getAttribute("data-key");
-            console.log(event.target.getAttribute("data-key"));
             $(`.gamerow[row=${pointer[0]}] .grid[col=${pointer[1]}]`).html('<p>' + letter+ '</p>'); 
-            $(`.gamerow[row=${pointer[0]}] .grid[col=${pointer[1]}]`).addClass('display'); 
+            $(`.gamerow[row=${pointer[0]}] .grid[col=${pointer[1]}]`).addClass('display slide-fwd-center'); 
             pointer[1]++;
+            guess.push(letter);
         }
     });
 
+    // while click on enter -> checkAnswer, next line
     $('.keyboard').on('click', '#enter', function(event) {
         event.preventDefault();
         if (pointer[1]>4){
+            checkAnswer(guess, answer);
             pointer[1] = 0;
+            guess = [];
             pointer[0]++;
         } else {
             $(`.gamerow[row=${pointer[0]}]`).addClass('animate__animated animate__headShake');
@@ -34,17 +51,17 @@ $(document).ready(function(){
         }
     });
 
+    // while click on delete -> remove style and text in grid
     $('.keyboard').on('click', '#delete', function(event) {
         event.preventDefault();
         if (pointer[1]<=5 & pointer[1]>=0){
             pointer[1]--;
             $(`.gamerow[row=${pointer[0]}] .grid[col=${pointer[1]}] p`).remove(); 
             $(`.gamerow[row=${pointer[0]}] .grid[col=${pointer[1]}]`).removeClass('display'); 
-            console.log(pointer[1]);
+            guess.pop();
         } else {
             pointer[1] = 0;
         }
     });
 
-    
 });
